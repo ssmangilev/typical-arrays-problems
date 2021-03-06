4b41a2100c421881b27bbbc6f7da005f9d6af08f
Sergey Mangilev <ssmangilev@gmail.com>
2021-03-06 18:24:44 +0100

    init: task typical array problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof(array) === 'undefined' || array.length == 0) {
+        return 0;
+    }
+    let min = array[0]
+    for (let i=1;i<array.length;i++) {
+        if (array[i]<min) {
+            min = array[i]
+        }
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof(array) === 'undefined' || array.length == 0) {
+        return 0;
+    }
+  let max = array[0]
+  for (let i=1;i<array.length;i++) {
+    if (array[i]>max) {
+        max = array[i]
+    }
+}
+return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof(array) === 'undefined' || array.length == 0) {
+        return 0;
+    }
+    let avg_sum = 0
+    for (let i=0;i<array.length;i++) {
+        avg_sum += array[i]
+    }
+    return avg_sum/array.length;
 }
+
